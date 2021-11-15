a409b423b47fbe434f888ba1a814a2118dac222c
sierra_davenport <srose.davenport@gmail.com>
2021-11-15 10:47:32 -0700

    new repo

---
+++ batter/game/brick.py
@@ -0,0 +1,9 @@
+from actor import Actor
+
+class Brick(Actor):
+    def __init__():
+        pass
+    def set_position(self, Point):
+        self._position = Point(10, 10)
+        self._position = Point(100, 10)
+        self._position = Point(200, 10)
